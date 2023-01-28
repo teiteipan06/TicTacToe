44519e3e35646b966ea570dd7b560bc10cdaa21c
teiteipan06 <y7321543@gmail.com>
2023-01-28 10:57:49 +0900

    game test

--- GameLauncher.java
+++ GameLauncher.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class GameLauncher {
   
   public static void main(String[] args) {
     System.out.println("******* Game Start *******");
     System.out.print("Player-X, enter your move (row[1-3],colum[1-3]): ");
-    
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+
+    TicTacToe t = new TicTacToe();
+    t.printooxx();
   }
 }
--- TicTacToe.java
+++ TicTacToe.java
@@ -4,4 +4,15 @@
 public class TicTacToe {
 
   int[][] ooxx = new int[3][3];
+
+  // ooxx = {{1,1,1},{1,1,1},{1,1,1}};
+
+  public void printooxx() {
+    for(int[] a : ooxx){
+      for(int b : a){
+        System.out.print(b);
+      }
+      System.out.println();
+    }
+  }
 } 

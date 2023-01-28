e43f8f53d15eea491aa56d9671c3d9760a1bfcce
teiteipan06 <y7321543@gmail.com>
2023-01-28 11:26:22 +0900

    the setting of game rule

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,14 +3,25 @@
  */
 public class TicTacToe {
 
+  // 0:""
+  // 1:O
+  // 2:X
   int[][] ooxx = new int[3][3];
 
   // ooxx = {{1,1,1},{1,1,1},{1,1,1}};
 
+  public void go(String input) {
+    // 1,2
+    String[] tmp_array = input.split(",");
+    // tmp_array[0]
+    ooxx[Integer.parseInt(tmp_array[0])-1][Integer.parseInt(tmp_array[1])-1] = 2;
+  }
+
+
   public void printooxx() {
     for(int[] a : ooxx){
       for(int b : a){
-        System.out.print(b);
+        System.out.print( b==2?"X":b==1?"O":"" );
       }
       System.out.println();
     }
